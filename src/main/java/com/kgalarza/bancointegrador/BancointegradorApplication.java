1c38a66299a95cf0d5c151aa8131cd019fa87c2e
Kevin Jordan Galarza Vera <kevin.galarza@sofka.local>
2024-12-12 11:44:19 -0500

    Init del proyecto

---
+++ src/main/java/com/kgalarza/bancointegrador/BancointegradorApplication.java
@@ -0,0 +1,13 @@
+package com.kgalarza.bancointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancointegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancointegradorApplication.class, args);
+	}
+
+}
