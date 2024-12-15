c6a9712f3ad6dac8bab732fa4e40e831cb15acb3
Nicolas Bentancor <nicolas.bentancor@sofka.com.co>
2024-12-14 23:42:11 -0300

    🌟 feat(file): Added FinancesProApplication.java and FinancesProApplicationTests.java files.

---
+++ src/main/java/org/example/financespro/FinancesProApplication.java
@@ -0,0 +1,22 @@
+package org.example.financespro;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesProApplication {
+    public static void main(String[] args) {
+        // Load .env file
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .load();
+
+        // Set environment variables
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+        SpringApplication.run(FinancesProApplication.class, args);
+    }
+}
---
+++ src/test/java/org/example/financespro/FinancesProApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.financespro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
