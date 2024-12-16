4014fcf382ba61abdd505a9e9b6f6eaa6ca1587b
Nicolas Bentancor <nicolas.bentancor@sofka.com.co>
2024-12-16 19:05:17 -0300

    Fix handling of null Account exception in findByAccountNumber.

--- src/main/java/org/example/financespro/repository/AccountRepository.java
+++ src/main/java/org/example/financespro/repository/AccountRepository.java
@@ -1,10 +1,9 @@
 package org.example.financespro.repository;
 
+import java.util.Optional;
 import org.example.financespro.model.Account;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
 public interface AccountRepository extends MongoRepository<Account, String> {
-    Optional<Account> findByAccountNumber(String accountNumber);
+  Optional<Account> findByAccountNumber(String accountNumber);
 }
