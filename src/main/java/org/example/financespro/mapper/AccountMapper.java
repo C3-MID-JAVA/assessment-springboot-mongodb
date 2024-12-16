1a9a9e67bbe0a05654480264e840f267423763bd
Nicolas Bentancor <nicolas.bentancor@sofka.com.co>
2024-12-16 19:04:27 -0300

    Fix AccountMapper to handle null Account exception.

--- src/main/java/org/example/financespro/mapper/AccountMapper.java
+++ src/main/java/org/example/financespro/mapper/AccountMapper.java
@@ -8,6 +8,10 @@ public class AccountMapper {
   private AccountMapper() {}
 
   public static AccountResponseDto toResponseDTO(Account account) {
+    if (account == null) {
+      throw new IllegalArgumentException("Account cannot be null");
+    }
+
     AccountResponseDto response = new AccountResponseDto();
     response.setId(account.getId());
     response.setAccountNumber(account.getAccountNumber());
