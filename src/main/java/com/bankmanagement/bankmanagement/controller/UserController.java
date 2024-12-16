e74d1d47977fa8e16784bf8a1a2a4a0e98ad6e30
Adrian de los Reyes <adrian.delosreyes@sofka.com.co>
2024-12-16 00:50:34 -0300

    build: swagger doc

--- src/main/java/com/bankmanagement/bankmanagement/controller/UserController.java
+++ src/main/java/com/bankmanagement/bankmanagement/controller/UserController.java
@@ -2,7 +2,14 @@ package com.bankmanagement.bankmanagement.controller;
 
 import com.bankmanagement.bankmanagement.dto.UserRequestDTO;
 import com.bankmanagement.bankmanagement.dto.UserResponseDTO;
+import com.bankmanagement.bankmanagement.exception.ErrorResponse;
 import com.bankmanagement.bankmanagement.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Users", description = "Operations related to users")
 @RestController
 @RequestMapping("users")
 public class UserController {
@@ -21,6 +29,27 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Register a new user",
+            description = "This endpoint registers a new user in the system. It accepts a request body with user details and returns the created user response with a unique user identifier.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "User successfully created",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request, validation error or missing required fields",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request, user already exist",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<UserResponseDTO> register(@Valid @RequestBody UserRequestDTO userRequestDTO){
         UserResponseDTO userResponseDTO = userService.register(userRequestDTO);
--- src/main/java/com/bankmanagement/bankmanagement/exception/ErrorResponse.java
+++ src/main/java/com/bankmanagement/bankmanagement/exception/ErrorResponse.java
@@ -1,15 +1,21 @@
 package com.bankmanagement.bankmanagement.exception;
 
-import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "Standard error response format")
 @Data
 public class ErrorResponse {
+
+    @Schema(description = "Http Status", example = "BAD_REQUEST")
     private HttpStatus status;
+
+    @Schema(description = "Error message providing more details", example = "The provided account number does not exist in the system.")
     private String error;
+
     private LocalDateTime timestamp;
 
     public ErrorResponse(HttpStatus status, String error) {
