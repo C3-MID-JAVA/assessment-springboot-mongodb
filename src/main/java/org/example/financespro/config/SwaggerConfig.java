9e56c1725b9ef55aff4c5634bab6274119b88fcd
Nicolas Bentancor <nicolas.bentancor@sofka.com.co>
2024-12-16 19:10:52 -0300

    Add Swagger configuration for API documentation in SwaggerConfig.java.

---
+++ src/main/java/org/example/financespro/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package org.example.financespro.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("FinancesPro API")
+                .version("1.0.0")
+                .description("API documentation for FinancesPro project"));
+  }
+}
